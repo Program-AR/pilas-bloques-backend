5649a8b98bccae79305b8bf621f76c1bf3fb0a25
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2023-01-05 18:32:04 -0300

    borrando carpeta al dope

--- pilas-bloques-analytics-consumer/test/persistence/describeDB.ts
+++
@@ -1,8 +0,0 @@
-import { disconnectDB, startDB } from '../utils'
-
-export default (name: string, tests: () => void) =>
-  describe(name, () => {
-    beforeAll(() => startDB())
-    afterAll(() => disconnectDB())
-    tests()
-  })
--- pilas-bloques-analytics-consumer/test/persistence/seed.spec.ts
+++
@@ -1,9 +0,0 @@
-import describeDB from './describeDB'
-import analizer from '../../src/persistence/analyzer'
-
-describeDB('Seed', () => {
-  test('Running on full db', async () => {
-    const count = await analizer.solutionsCount()
-    expect(count).toBeGreaterThan(0)
-  })
-})
