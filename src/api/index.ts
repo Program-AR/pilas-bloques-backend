988f29f18d236ee13fc80702a577eb1a341f36b4
Daniel Ferro <danieloscarferro@gmail.com>
2023-04-26 10:23:59 -0300

    patch-cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import * as express from 'express'
 // ENV VAR
 import * as dotenv from 'dotenv'
 import * as dotenvExpand from 'dotenv-expand'
+import * as cors from 'cors'
 const myEnv = dotenv.config()
 dotenvExpand.expand(myEnv)
 console.log(myEnv)
@@ -29,6 +30,23 @@ const port = process.env.API_PORT
 const app = express()
 app.set('trust proxy', true) // If true, the client’s IP address is understood as the left-most entry in the X-Forwarded-* header.
 app.use(configMailing(transport))
+
+var whitelist = ['http://localhost:4200']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials:true,
+  //allowedHeaders: 'Content-Type,Authorization',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+}
+
+app.use(cors(corsOptions))
 app.use(routes)
 app.listen(port, () => {
   log(`Server started at ${port}`)
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,5 +1,4 @@
 import * as express from 'express'
-import * as cors from 'cors'
 import serverErrorHandler from './errorHandlers'
 import auth from './auth'
 import solutions from './solutions'
@@ -8,7 +7,6 @@ import userIp from './ip'
 const router = express.Router()
 
 router.use(express.json())
-router.use(cors())
 router.all('/ping', (_, res) => res.send('pong'))
 router.use(auth)
 router.use(solutions)
