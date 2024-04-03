70098e6c03f9462dc111ee8c4c4502283ae84630
Diana López Alvas <dlopezalvas@gmail.com>
2024-04-03 15:32:19 -0300

    password recovery

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,8 +1,8 @@
 import * as express from 'express'
-import { syncHandler, ofuscate, AuthenticatedRequest } from './utils'
-import { generatePassword, verifyPassword, newToken } from '../models/auth'
+import { syncHandler, AuthenticatedRequest } from './utils'
+import { generatePassword, verifyPassword, newToken, parseToken } from '../models/auth'
 import { tokenAuth, requiredBody, requiredQueryParams, passwordChangeAuth } from './middlewares'
-import { HttpCodeError, WrongCredentials } from './errorHandlers'
+import { WrongCredentials } from './errorHandlers'
 import { passwordRecoveryMail } from '../mailing/mails'
 import { User, UserModel } from '../../models/user'
 
@@ -13,7 +13,7 @@ const toJsonUser = (user: User) => ({ id: user._id, token: newToken(user,LOGIN_E
 const router = express.Router()
 
 router.post('/register', requiredBody('username', 'password'), syncHandler(async ({ body }: AuthenticatedRequest, res) => {
-  const username = UserModel.standarizeUsername(body.username)
+  const username = UserModel.standarizeIdentifier(body.username)
   const experimentGroup = body.context.experimentGroup
   const user = await UserModel.create({ ...body, experimentGroup, username, ...generatePassword(body.password) })
   res.json(toJsonUser(user))
@@ -30,11 +30,13 @@ router.put('/credentials', requiredBody('password'), passwordChangeAuth, syncHan
   res.json(toJsonUser(user))
 }))
 
-router.post('/password-recovery', requiredQueryParams('username'), syncHandler(async ({ query, transport }: AuthenticatedRequest, res) => {
-  const user = await UserModel.findByUsername(query['username'] as string).exec()
-  if (!user) throw new HttpCodeError(404, "User does not exist")
-  if (user.email) await transport.sendMail(passwordRecoveryMail(user))
-  res.json({ email: user.email ? ofuscate(user.email) : null })
+router.post('/password-recovery', requiredQueryParams('userIdentifier'), syncHandler(async ({ query, transport }: AuthenticatedRequest, res) => {
+  const userIdentifier = query['userIdentifier'] as string
+  const identifierIsMail = userIdentifier.includes('@')
+  const user = identifierIsMail ? await UserModel.findByEmail(userIdentifier).exec() : await UserModel.findByUsername(userIdentifier).exec()
+  
+  if (user && user.email) await transport.sendMail(passwordRecoveryMail(user))
+  res.json(toJsonUser(user))
 }))
 
 router.post('/answers', tokenAuth, requiredBody('question', 'response'), syncHandler(async ({ user, body }: AuthenticatedRequest, res) => {
@@ -55,6 +57,13 @@ router.put('/experiment-group', tokenAuth, requiredBody('group'), syncHandler(as
 
 router.get('/users/exists', requiredQueryParams('username'), syncHandler(async ({ query }: AuthenticatedRequest, res) => {
   const user = await UserModel.findByUsername(query['username'] as string).exec()
+  res.json(Boolean(user))
+}))
+
+router.post('/valid-token', requiredQueryParams('token'), syncHandler(async ({ query }: AuthenticatedRequest, res) => {
+  const { id } = parseToken(query['token'] as string)
+  const user = await UserModel.findById(id).exec()
+  
   res.json(Boolean(user))
 }))
 
